7e3fcad20c2396c490f2bff93a0bcd3954b740eb
MikeTheFyke <mikefyke@hotmail.com>
2020-10-21 09:00:54 -0400

    Added stlying to FoodItem.vue

--- src/components/FoodItem.vue
+++ src/components/FoodItem.vue
@@ -12,4 +12,23 @@ export default {
 
 <style scoped>
 
+.food-item{
+    background: #f4f4f4;
+    padding: 10px;
+    border-bottom: 1px #ccc dotted;
+}
+
+.is-complete{
+    text-decoration: line-through;
+}
+
+.del{
+    background: #ff0000;
+    color: #fff;
+    padding: 5px 9px;
+    border-radius: 50%;
+    cursor: pointer;
+    float: right;
+}
+
 </style>
