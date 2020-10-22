47ed35793c7b9b3bab4efa7f5a64095a502383e2
MikeTheFyke <mikefyke@hotmail.com>
2020-10-22 12:40:41 -0400

    Updated get with mongodb credentials

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -4,13 +4,24 @@ const mongodb = require('mongodb');
 const router = express.Router();
 
 // GET Posts
-router.get('/', (req, res) => {
-    res.send("Hello");
+router.get('/', async (req, res) => {
+    const posts = await loadPostsCollection();
+    res.send(await posts.find({}).toArray());
 })
 
 // ADD Posts
 
 
 // DELETE Post
+
+
+// Connection to MongoDB
+async function loadPostsCollection() {
+    const client = await mongodb.MongoClient.connect
+    ('mongodb+srv://mike_123:mike123@cluster0.r3pvv.mongodb.net/vue_bytes?retryWrites=true&w=majority', {
+        useNewUrlParser: true
+    });
+    return client.db('vue_bytes').collection('posts');
+}
 
 module.exports = router;
