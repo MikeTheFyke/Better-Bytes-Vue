1c526c020839e4aba425c753d9e391c2958b59fb
MikeTheFyke <mikefyke@hotmail.com>
2020-10-22 12:49:31 -0400

    Added delete method to posts route

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -12,14 +12,19 @@ router.get('/', async (req, res) => {
 // ADD Posts
 router.post('/', async (req, res) => {
     const posts = await loadPostsCollection();
-    await this.post.insertOne({
+    await posts.insertOne({
         text: req.body.text,
         createdAt: new Date()
     });
-    res.status(201).send;
+    res.status(201).send();
 });
 
 // DELETE Post
+router.delete('/:id', async (req, res) => {
+    const posts = await loadPostsCollection();
+    await posts.deleteOne({_id: new mongodb.ObjectID (req.params.id)})
+    res.status(200).send({});
+})
 
 
 // Connection to MongoDB
