df79cbb9558bb95b5f6855adf3635fa2a7f0ff51
MikeTheFyke <mikefyke@hotmail.com>
2020-10-22 15:32:40 -0400

    new Items are displaying on frontend

--- client/src/components/AddFood.vue
+++ client/src/components/AddFood.vue
@@ -4,6 +4,14 @@
             <input type="text" v-model="name" name="name" placeholder="Add New Food..." class="foodInput"  autocomplete="off">
             <input type="submit" value="Submit" class="btn" >
         </form>
+        <div class="container">
+        <h1> Lastest Items </h1>
+            <div class="posts-container">
+                <div class="post" v-for="post in posts" v-bind:item="post" v-bind:key="post._id">
+                    <p class="text">{{post.name}}</p>
+                </div>
+            </div>
+        </div>
     </div>    
 </template>
 
@@ -15,16 +23,18 @@ export default {
     data(){
         return{
             posts: [],
+            error: '',
             name: '',
             price: '$0.00',
             completed: false
         }
     },
+    // created ia a life cycle method
         async created () {
         try {
-        this.posts = await PostService.getPosts();
+            this.posts = await PostService.getPosts();
         } catch(err){
-        this.error = err.message;
+            this.error = err.message;
         }
     },
     methods: {
@@ -32,14 +42,14 @@ export default {
             await PostService.insertPost(this.name);
             this.posts = await PostService.getPosts();
             // constructing newly added food.
-            const newFood = {
-                // id: v4(),
-                name: this.name,
-                price: "$0.00",
-                completed: false
-            }
+            // const newFood = {
+            //     // id: v4(),
+            //     name: this.name,
+            //     price: "$0.00",
+            //     completed: false
+            // }
             // send up to parent
-            this.$emit('add-food', newFood);
+            // this.$emit('add-food', newFood);
             // clears form
             this.name = '';
         }
