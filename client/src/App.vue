77cea7cb4a3b5f9482ae22fd552f5e2c62dd2905
MikeTheFyke <mikefyke@hotmail.com>
2020-10-25 18:31:49 -0400

    Removed tweenMax from App.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -64,11 +64,6 @@ export default {
   //   }
   // },
   methods: {
-            mounted() {
-              window.addEventListener('load', () => {
-                  Tweenmax.to("Header", 0.5, { y: '100px', delay: 0.5})
-              })
-            }
     // deleteFood(id){
     //   // filter and keep any id that doesnt match id that is passed to this method.
     //   this.foods = this.foods.filter(food => food.id !== id);
